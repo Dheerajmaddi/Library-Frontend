d11b53bb9ef29aa8e01b02084d38d2857bf80ec2
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-02 22:18:10 -0500

    Added NavLink to home and search in the NavBar component.

--- src/layouts/NavbarAndFooter/NavigationBar.tsx
+++ src/layouts/NavbarAndFooter/NavigationBar.tsx
@@ -1,27 +1,43 @@
+import { NavLink } from "react-router-dom";
+
 export const Navbar = () => {
-    return (
-        <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
-      <div className='container-fluid'>
-        <span className='navbar-brand'>Luv 2 Read</span>
-        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropDown' aria-controls='navbarNavDropDown' aria-expanded='false' aria-label='Toggle Navigation'>
-          <span className='navbar-toggler-icon'></span>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
+      <div className="container-fluid">
+        <span className="navbar-brand">Luv 2 Read</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropDown"
+          aria-controls="navbarNavDropDown"
+          aria-expanded="false"
+          aria-label="Toggle Navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarNavDropDown'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <a href="#" className='nav-link'>Home</a>
+        <div className="collapse navbar-collapse" id="navbarNavDropDown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/home" className="nav-link">
+                Home
+              </NavLink>
             </li>
-            <li className='nav-item'>
-              <a href="#" className='nav-link'>Search Books</a>
+            <li className="nav-item">
+              <NavLink to="/search" className="nav-link">
+                Search Books
+              </NavLink>
             </li>
           </ul>
-          <ul className='navbar-nav ms-auto'>
-            <li className='nav-item m-1'>
-              <a href="#" type='button' className='btn btn-outline-light'>Sign in</a>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item m-1">
+              <a href="#" type="button" className="btn btn-outline-light">
+                Sign in
+              </a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-    );
-}
+  );
+};
