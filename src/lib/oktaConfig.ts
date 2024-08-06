49a991f1f6e4a4f1c96388a1ba4a333ada0e25a3
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-06 13:05:57 -0500

    Added a new folder which contains okta configuration file.

---
+++ src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const OktaConfig = {
+    clientId: "0oaisup9ow0Twspas5d7",
+    issuer: "https://dev-47165213.okta.com/oauth2/default",
+    redirectUri: "http://localhost:3000/login/callback",
+    scopes: ["openid", "profile", "email"],
+    pkce: true,
+    disableHttpsChecks: true
+}
