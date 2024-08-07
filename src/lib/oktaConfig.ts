a909e88ddc80415aed976429f68832bb00b44f78
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-06 21:18:30 -0500

    Added a different Okta account and changed the configuration accirgingly.

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,8 +1,21 @@
+
+// Okta Login : https://dev-47165213.okta.com/
+// export const oktaConfig = {
+//     clientId: "0oaisup9ow0Twspas5d7",
+//     issuer: "https://dev-47165213.okta.com/oauth2/default",
+//     redirectUri: "http://localhost:3000/login/callback",
+//     scopes: ["openid", "profile", "email"],
+//     pkce: true,
+//     disableHttpsChecks: true
+// }
+
 export const oktaConfig = {
-    clientId: "0oaisup9ow0Twspas5d7",
-    issuer: "https://dev-47165213.okta.com/oauth2/default",
+    clientId: "0oait5breyTNVKYoS5d7",
+    issuer: "https://dev-04591174.okta.com/oauth2/default",
     redirectUri: "http://localhost:3000/login/callback",
     scopes: ["openid", "profile", "email"],
     pkce: true,
     disableHttpsChecks: true
 }
+// Okta Login : https://dev-04591174.okta.com/
+
