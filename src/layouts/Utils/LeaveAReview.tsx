0f7148d7270dc81d9c36fd9a6a17ab2667312ef5
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-11 17:56:06 -0500

    Forgot to pass arguements to the submit review function on the submit button. Fixed it!!!!.

--- src/layouts/Utils/LeaveAReview.tsx
+++ src/layouts/Utils/LeaveAReview.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { StarsReview } from "./StarsReview";
 
-export const LeaveAReview: React.FC<{}> = (props) => {
+export const LeaveAReview: React.FC<{ submitReview: any }> = (props) => {
   const [starInput, setStarInput] = useState(0);
   const [displayInput, setDisplayInput] = useState(false);
   const [reviewDescription, setReviewDescription] = useState("");
@@ -91,7 +91,6 @@ export const LeaveAReview: React.FC<{}> = (props) => {
             <label className="form-label">Description</label>
             <textarea
               className="form-control"
-              name=""
               id="submitReviewDescription"
               placeholder="Optional"
               rows={3}
@@ -99,7 +98,11 @@ export const LeaveAReview: React.FC<{}> = (props) => {
             ></textarea>
           </div>
           <div>
-            <button type="button" className="btn btn-primary mt-3">
+            <button
+              type="button"
+              className="btn btn-primary mt-3"
+              onClick={() => props.submitReview(starInput, reviewDescription)}
+            >
               Submit Review
             </button>
           </div>
