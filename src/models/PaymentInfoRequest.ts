6e8e5d20dc2acc42167fa5d57c11fa17604a55f5
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-22 23:59:55 -0500

    Fixed a bug in the constructor paramters to include undefines type.

--- src/models/PaymentInfoRequest.ts
+++ src/models/PaymentInfoRequest.ts
@@ -3,7 +3,7 @@ class PaymentInfoRequest {
     currency: string;
     receiptEmail: string | undefined;
 
-    constructor(amount: number, currency: string, receiptEmail: string) {
+    constructor(amount: number, currency: string, receiptEmail: string | undefined) {
         this.amount = amount;
         this.currency = currency;
         this.receiptEmail = receiptEmail;
